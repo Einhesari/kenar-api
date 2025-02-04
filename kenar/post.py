f8e03c1eb666cfc40aa9bfa283730eb6bf059e1c
Mohsen Einhesari <34568345+Einhesari@users.noreply.github.com>
2025-02-04 18:56:47 +0330

    Fix the bug in dict_excluding_none function of EditPostRequest

--- kenar/post.py
+++ kenar/post.py
@@ -9,7 +9,7 @@ class EditPostRequest(BaseModel):
     image_paths: Optional[list] = None
 
     def dict_excluding_none(self) -> Dict[str, Any]:
-        return {k: v for k, v in self.model_dump() if v is not None}
+        return {k: v for k, v in self.model_dump().items() if v is not None}
 
 
 class EditPostResponse(BaseModel):
